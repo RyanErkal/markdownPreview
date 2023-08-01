50011da399065d75c9db76f2af1fdb36100c96d1
Ryan Erkal <ryanerkal@yahoo.ie>
2023-08-01 09:17:50 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MarkdownPreview from "./MarkdownPreview";
+
+function App() {
+	return (
+		<div className="App">
+			<MarkdownPreview />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/MarkdownPreview.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function MarkdownPreview() {
+	return <h1>Markdown</h1>;
+}
+
+export default MarkdownPreview;
